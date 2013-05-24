8d53f1f4f4bf8c69cf361292f1c1ab11346e0a03
KuznetsovSergey <kuznecov_sergey@mail.ru>
2013-05-24 19:41:12 +0300

    first commit

--- spec/requests/routes_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-describe "Routes" do 
-	describe "Home page" do
-		it "should have the title 'Home'" do
-			visit "/"
-			page.should have_selector('title',text: "Home" ) 
-		end
-	end
-end
